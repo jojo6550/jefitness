cd416867433ab98fd3aa20179b1cea70b4246d16
jojo6550 <josiah.johnson6550@gmail>
2025-12-06 20:58:45 -0500

    tailwind works

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/pages/*.html",        // all HTML files in project root
+    "./src/**/*.js",   // all JS files in src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
