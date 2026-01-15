f1f2522ee4c34dd2c9a6c64ee9604a16b576afae
jojo6550 <josiah.johnson6550@gmail>
2026-01-14 22:38:44 -0500

    37

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 // ===============================
-// JEFitness Service Worker (v36)
+// JEFitness Service Worker (v37)
 // ===============================
 
 // Cache versioning
-const CACHE_VERSION = '36';
+const CACHE_VERSION = '37';
 const STATIC_CACHE = `jefitness-static-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `jefitness-dynamic-v${CACHE_VERSION}`;
 
