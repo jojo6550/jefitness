5db112f358abe4f511faa820516853107242ca33
jojo6550 <josiah.johnson6550@gmail>
2026-01-14 23:02:51 -0500

    `Update service worker version to v40`

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,9 @@
 // ===============================
-// JEFitness Service Worker (v39)
+// JEFitness Service Worker (v40)
 // ===============================
 
 // Cache versioning
-const CACHE_VERSION = '39';
+const CACHE_VERSION = '40';
 const STATIC_CACHE = `jefitness-static-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `jefitness-dynamic-v${CACHE_VERSION}`;
 
