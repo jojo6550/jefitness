d3c13d7a32247b9d11ee94e1dee65de3cc0e384b
jojo6550 <josiah.johnson6550@gmail>
2026-01-16 10:37:19 -0500

    trainer update

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // Service Worker Registration for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('../sw.js')
       .then(registration => {
         console.log('SW registered: ', registration);
       })
