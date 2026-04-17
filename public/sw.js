bd6bb5f0b1eb3b525f9c78f6433b3d56f6aebdcf
jojo6550 <josiah.johnson6550@gmail>
2026-04-16 21:35:57 -0500

    chore:cache bust

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,7 @@
 let bypassCacheEnabled = false;
 
 // Cache versioning
-const CACHE_VERSION = '1aa870d';
+const CACHE_VERSION = '8da4051';
 const STATIC_CACHE = `jefitness-static-v${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `jefitness-dynamic-v${CACHE_VERSION}`;
 
